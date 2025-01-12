add9a503e94d20c955ee72a6c7eed43bbcef7029
phanxuanchanh <contact@phanxuanchanh.com>
2025-01-12 15:31:44 +0700

    Add ADO.NET project

---
+++ khothemegiatot.ADO.NET/AsyncSqlExecHelper.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.Common;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace khothemegiatot.ADO.NET;
+
+public partial class SqlExecHelper
+{
+    public async Task ConnectAsync()
+    {
+        if (_connection != null && _connection.State != ConnectionState.Open)
+            await _connection.OpenAsync();
+    }
+
+    public void DisconnectAsync()
+    {
+        if (_connection != null && _connection.State != ConnectionState.Closed)
+            _connection.Close();
+    }
+
+    public async IAsyncEnumerable<T> ExecuteReaderAsync<T>(string sql, Dictionary<string, object> parameters, Func<DbDataReader, T> mapper)
+    {
+        await ConnectAsync();
+
+        using SqlCommand cmd = new SqlCommand(sql, _connection);
+        cmd.CommandType = CommandType.Text;
+
+        if (parameters != null)
+            parameters = new Dictionary<string, object>();
+
+        foreach (var (key, value) in parameters)
+            cmd.Parameters.AddWithValue(key, value);
+
+        using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            yield return mapper(reader);
+        }
+    }
+
+    public async Task<object> ExecuteScalarQueryAsync<Tscalar>(string sql, Dictionary<string, object> parameters)
+    {
+        await ConnectAsync();
+
+        using SqlCommand cmd = new SqlCommand(sql, _connection);
+        cmd.CommandType = CommandType.Text;
+
+        if (parameters != null)
+            parameters = new Dictionary<string, object>();
+
+        foreach (var (key, value) in parameters)
+            cmd.Parameters.AddWithValue(key, value);
+
+        return await cmd.ExecuteScalarAsync();
+    }
+
+    public async Task<int> ExecuteNonQueryAsync(string sql, Dictionary<string, object> parameters)
+    {
+        await ConnectAsync();
+
+        using SqlCommand cmd = new SqlCommand(sql, _connection);
+        cmd.CommandType = CommandType.Text;
+
+        if (parameters != null)
+            parameters = new Dictionary<string, object>();
+
+        foreach (var (key, value) in parameters)
+            cmd.Parameters.AddWithValue(key, value);
+
+        return await cmd.ExecuteNonQueryAsync();
+    }
+}
---
+++ khothemegiatot.ADO.NET/DaoBase.cs
@@ -0,0 +1,36 @@
+﻿namespace khothemegiatot.ADO.NET;
+
+public class DaoBase<T> : IDisposable
+{
+    protected readonly SqlExecHelper sqlExecHelper;
+    private bool disposedValue;
+
+    public DaoBase(SqlExecHelper sqlExecHelper)
+    {
+        _sqlExecHelper = sqlExecHelper;
+    }
+
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
